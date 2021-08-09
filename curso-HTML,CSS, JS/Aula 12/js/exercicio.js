74c1a11b4137f7aa788e122eedfb215b50f21cdf
Andreas <andreashgrings@gmail.com>
2021-08-09 14:39:04 -0300

    exercicio aula 12

---
+++ curso-HTML,CSS, JS/Aula 12/js/exercicio.js
@@ -0,0 +1,36 @@
+var x;
+var y; 
+var z
+var media;
+alert ('Coloque as suas notas para saber a média')
+
+
+x= prompt ('Informe a primeira nota:');
+x= eval (x);
+
+y= prompt ('Informe a segunda nota:');
+y= eval (y);
+
+z= prompt ('Informe a terceira nota');
+z= eval (z)
+
+
+media = (x + y +z)/3; 
+
+if ( media >= 7 ) 
+{
+    alert('Aprovado');
+} 
+else { 
+    alert('Reprovado');
+}
+
+var nome;
+var salário;
+
+nome= prompt ('Escreva seu nome')
+salário= prompt ('Escreva seu salário:')
+alert (salário*1.1)
+
+
+
